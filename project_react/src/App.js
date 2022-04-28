5534a688cf586d31930b92b150f9bbe75f5463ef
Gabriel-Nog <nogueirajoao799@gmail.com>
2022-04-28 08:36:10 -0300

    Criação das rotas bases e do container

--- project_react/src/App.js
+++ project_react/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layuot/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <Router>
+        <div>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/company'}>Sobre</Link>
+          <Link to={'/contact'}>Contato</Link>
+          <Link to={'/newproject'}>Novo Projeto</Link>
+        </div>
+        <Container customClass="min-height">
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/company' element={<Company />} />
+            <Route exact path='/contact' element={<Contact />} />
+            <Route exact path='/newproject' element={<NewProject />} />
+          </Routes>
+        </Container>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Footer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      </Router>
+    </>
   );
 }
 
---
+++ project_react/src/components/pages/Company.js
@@ -0,0 +1,7 @@
+function Company() {
+    return (
+        <h1>Compania</h1>
+    )
+}
+
+export default Company;
